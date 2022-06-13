f30c9404960fde814beb0b7149628d8ba0a63f45
Alfredo <alfredofontinele08@gmail.com>
2022-06-13 14:08:34 -0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Jhon";
+let sobreNome = "Doyle";
+let anoNascimento = 1991;
+let cidade = "Curitiba";
+let anoAtual = 2022;
+
+let idade = anoAtual - anoNascimento;
+
+console.log(`\nEu sou ${nome} ${sobreNome}, tenho ${idade} anos e moro em ${cidade}\n`);
